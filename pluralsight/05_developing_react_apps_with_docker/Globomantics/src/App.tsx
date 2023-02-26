cfd8923b4441f4be04ba4c9b4167483d244e9aa2
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-02-26 11:51:37 +0100

    Created starter app

--- pluralsight/05_developing_react_apps_with_docker/Globomantics/src/App.tsx
+++ pluralsight/05_developing_react_apps_with_docker/Globomantics/src/App.tsx
@@ -1,11 +1,46 @@
-import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>Globomantics</h1>
-    </>
+    <div className='App'>
+      <body>
+        <div className='container'>
+          <div className='row well mb-3'>
+            <div className='col-md-3 col-sm-3'>
+              <button
+                type='button'
+                className='btn btn-secondary btn-lg disabled'
+              >
+                Add Inventory
+              </button>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <button
+                type='button'
+                className='btn btn-secondary btn-lg disabled'
+              >
+                Check Inventory
+              </button>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <button
+                type='button'
+                className='btn btn-secondary btn-lg disabled'
+              >
+                Sell Items
+              </button>
+            </div>
+          </div>
+
+          <div className='row well mb-3'>
+            <div className='col-md-6'>
+              <h5>There are 8 items in the inventory!!</h5>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
   );
 };
 
