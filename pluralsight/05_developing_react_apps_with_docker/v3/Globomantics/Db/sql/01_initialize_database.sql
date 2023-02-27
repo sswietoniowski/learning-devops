05386bbc58c91d049db3802de2094af20a6f7cee
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-02-27 13:26:32 +0100

    Updated SQL code

--- pluralsight/05_developing_react_apps_with_docker/v3/Globomantics/Db/sql/01_initialize_database.sql
+++ pluralsight/05_developing_react_apps_with_docker/v3/Globomantics/Db/sql/01_initialize_database.sql
@@ -5,10 +5,6 @@ CREATE TABLE IF NOT EXISTS inventory
 );
 
 INSERT INTO inventory (amount, sku)
-SELECT 
-    1 AS sku,
-    100 AS amount
-WHERE
-    NOT EXISTS (SELECT 1 FROM inventory WHERE sku = 1);
-
+VALUES (1, 200)
+ON CONFLICT (sku) DO NOTHING;
 
