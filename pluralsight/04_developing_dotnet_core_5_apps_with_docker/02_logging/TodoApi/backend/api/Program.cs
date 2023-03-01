bad9d9908b3ba7b38b0e7e7cb1557f1e2b5c2564
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-03-01 13:17:20 +0100

    Configured project to use Serilog

--- pluralsight/04_developing_dotnet_core_5_apps_with_docker/02_logging/TodoApi/backend/api/Program.cs
+++ pluralsight/04_developing_dotnet_core_5_apps_with_docker/02_logging/TodoApi/backend/api/Program.cs
@@ -1,83 +1,101 @@
-using Polly;
+using api.Configurations.Middleware;
+
 using backend.api;
 
 using Microsoft.EntityFrameworkCore;
-using api.Configurations.Middleware;
+
+using Polly;
+
 using Serilog;
+using Serilog.Events;
 
 Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.Console()
-            .WriteTo.File("Logs/logs.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseSerilog();
-
-var databaseEngine = builder.Configuration.GetValue<DatabaseEngine>("DatabaseEngine");
-
-if (databaseEngine == DatabaseEngine.Sqlite)
+try
 {
-    builder.Services.AddDbContext<SqliteTodoContext>(options =>
+    Log.Information("Starting web host");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    var databaseEngine = builder.Configuration.GetValue<DatabaseEngine>("DatabaseEngine");
+
+    if (databaseEngine == DatabaseEngine.Sqlite)
     {
-        options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
-    });
-    builder.Services.AddScoped<BaseTodoContext>(provider => provider.GetRequiredService<SqliteTodoContext>());
-}
-else if (databaseEngine == DatabaseEngine.Mssql)
-{
-    builder.Services.AddDbContext<MssqlTodoContext>(options =>
-    {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MssqlConnection"));
-    });
-    builder.Services.AddScoped<BaseTodoContext>(provider => provider.GetRequiredService<MssqlTodoContext>());
-}
-else 
-{
-    throw new InvalidOperationException("Invalid database engine");
-}
-
-builder.Services.AddAutoMapper(typeof(TodoProfile));
-builder.Services.AddScoped<ITodoService, TodoService>();
-
-builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-const int MAX_RETRIES = 7;
-const int RETRY_DELAY_IN_SECONDS = 15;
-var retryPolicy = Policy.Handle<Exception>()
-	.WaitAndRetryAsync(retryCount: MAX_RETRIES, 
-        sleepDurationProvider: (attemptCount) => TimeSpan.FromSeconds(RETRY_DELAY_IN_SECONDS));
-
-await retryPolicy.ExecuteAsync(async () =>
-{
-    var context = (app as IApplicationBuilder)
-        ?.ApplicationServices
-        .CreateScope().ServiceProvider
-        .GetRequiredService<BaseTodoContext>() 
-        ?? throw new InvalidOperationException("Invalid application builder");
-
-    if ((await context.Database.GetPendingMigrationsAsync()).Any())
-    {
-        await context.Database.MigrateAsync();
+        builder.Services.AddDbContext<SqliteTodoContext>(options =>
+        {
+            options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+        });
+        builder.Services.AddScoped<BaseTodoContext>(provider => provider.GetRequiredService<SqliteTodoContext>());
     }
-});
+    else if (databaseEngine == DatabaseEngine.Mssql)
+    {
+        builder.Services.AddDbContext<MssqlTodoContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("MssqlConnection"));
+        });
+        builder.Services.AddScoped<BaseTodoContext>(provider => provider.GetRequiredService<MssqlTodoContext>());
+    }
+    else
+    {
+        throw new InvalidOperationException("Invalid database engine");
+    }
 
-app.Run();
+    builder.Services.AddAutoMapper(typeof(TodoProfile));
+    builder.Services.AddScoped<ITodoService, TodoService>();
+
+    builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddControllers();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.MapControllers();
+
+    const int MAX_RETRIES = 7;
+    const int RETRY_DELAY_IN_SECONDS = 15;
+    var retryPolicy = Policy.Handle<Exception>()
+        .WaitAndRetryAsync(retryCount: MAX_RETRIES,
+            sleepDurationProvider: (attemptCount) => TimeSpan.FromSeconds(RETRY_DELAY_IN_SECONDS));
+
+    await retryPolicy.ExecuteAsync(async () =>
+    {
+        var context = (app as IApplicationBuilder)
+                      ?.ApplicationServices
+                      .CreateScope().ServiceProvider
+                      .GetRequiredService<BaseTodoContext>()
+                      ?? throw new InvalidOperationException("Invalid application builder");
+
+        if ((await context.Database.GetPendingMigrationsAsync()).Any())
+        {
+            await context.Database.MigrateAsync();
+        }
+    });
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+    System.Environment.Exit(1);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
